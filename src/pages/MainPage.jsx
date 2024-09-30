99d9725f764fb3eb5714b4c14e6c37e318104d62
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2024-09-30 19:23:24 +0600

    main page padding x

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -8,7 +8,7 @@ export default function MainPage() {
       <Navbar>
         <NavLink to='login' className='button__outline--primary px-6 py-3 rounded-md text-lg font-semibold hover:border-white'>Login</NavLink>
       </Navbar>
-      <section className="min-h-lvh bg--page grid place-items-center">
+      <section className="min-h-lvh bg--page grid place-items-center px-2">
         <div>
           <h1 className="text-4xl mv-3">Do You want to login or register</h1>
           <Link to='/bss-restaurant-app/login' className="button-primary rounded-lg font-bold text-4xl block text-center px-6 py-3 w-full mb-3">Login</Link>
